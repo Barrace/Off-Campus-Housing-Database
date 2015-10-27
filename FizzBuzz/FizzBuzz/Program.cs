a08eed816942374ad1251b8672a4e77db41f40bc
bathks52 <bathks52@uwosh.edu>
2015-10-27 12:47:29 -0500

    v5.0

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -10,7 +10,7 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("START");
+            System.Console.WriteLine("HELLO");
             int i;
             for (i = 1; i <= 100; i++)
             {
